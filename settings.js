dccff01cd73daf88dc01453c0312cbb5351a2079
RealChrisXd <chimbikouzukwu202@gmail.com>
2025-05-14 16:24:40 +0100

    Update and rename app.json to settings.js

---
+++ settings.js
@@ -0,0 +1,11 @@
+module.exports = {
+  ownerName: "Chimbiko-xd",
+  botName: "Lite_Chris-X",
+  ownerNumber: ["2348069675806@s.whatsapp.net"], // Your number
+  sessionName: "session",
+  prefix: ["!","."],
+  autoread: true,
+  antibadword: false,
+  antiLink: false
+};
+  
